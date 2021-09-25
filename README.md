fc73d5ffbb1e62479e7d45ba7c3e42bd1546100c
Tim Roberts <ten3roberts@gmail.com>
2021-09-26 00:08:10 +0200

    Inital commit (Working)

---
+++ README.md
@@ -0,0 +1,22 @@
+# derive-for
+A macro for defining multiple structs using the same derive procedures.
+
+The most common use case is in combination with the [`derive_more`](https://crates.io/crates/derive_more) for using the
+same derives to create transparent newtypes.
+
+## Usage
+```rust
+derive_for!(
+( Clone, Debug, PartialEq, Eq)
+pub struct Foo{a: i32, name: String};
+pub struct Bar(u32, u32);
+);
+```
+
+`Clone`, `Debug`, `PartialEq`, and `Eq` will now be implemented for both
+`Foo` and `Bar`. If deriving many traits for many newtypes this significantly
+shortens the code.
+
+## State
+This crate is very small and feature complete. If there are any issues, feel
+free to open an issue.
---
+++ src/lib.rs
@@ -0,0 +1,76 @@
+/// Defines several structs each using the same derive procedures.
+/// ## Example
+/// ```
+/// #[macro_use] extern crate derive_for;
+/// derive_for!(
+/// ( Clone, Debug, PartialEq, Eq)
+/// pub struct Foo{a: i32, name: String};
+/// pub struct Bar(u32, u32);
+/// );
+/// ```
+#[macro_export]
+macro_rules! derive_for {
+// Derive tuple
+(($b:ident $(,$a:ident)*) $vis:vis struct $name:ident($($fields:tt)*); $($rest:tt)*) => {
+#[derive($($a,)* $b)]
+$vis struct $name($($fields)*);
+$crate::derive_for!( ($b, $($a,)*) $($rest)*);
+};
+// Derive tuple with trailing derive comma
+(($b:ident $(,$a:ident)*,) $vis:vis struct $name:ident($($fields:tt)*); $($rest:tt)*) => {
+#[derive($($a,)* $b)]
+$vis struct $name($($fields)*);
+$crate::derive_for!( ($b, $($a,)*) $($rest)*);
+};
+// Derive struct
+(($b:ident $(,$a:ident)*) $vis:vis struct $name:ident{ $($fields:tt)* }; $($rest:tt)*) => {
+#[derive($($a,)* $b)]
+$vis struct $name{ $($fields)* }
+$crate::derive_for!( ($b, $($a,)*) $($rest)*);
+};
+// Derive struct with trailing derive comma
+(($b:ident $(,$a:ident)*,) $vis:vis struct $name:ident{ $($fields:tt)* }; $($rest:tt)*) => {
+#[derive($($a,)* $b)]
+$vis struct $name{ $($fields)* }
+$crate::derive_for!( ($b, $($a,)*) $($rest)*);
+};
+(($($_:ident,)*)) => {};
+}
+
+#[cfg(test)]
+#[macro_use]
+mod tests {
+    use crate::derive_for;
+
+    derive_for!(
+    ( Clone, Debug, PartialEq, Eq)
+    pub struct Foo{a: i32, name: String};
+    pub struct Bar(u32, u32);
+        );
+
+    #[test]
+    pub fn test() {
+        let foo = Foo {
+            a: 5,
+            name: "Hello".into(),
+        };
+        let bar = Bar(3, 6);
+
+        // Debug
+        println!("{:?}, {:?}", foo, bar);
+
+        // Eq
+        assert_eq!(
+            foo,
+            Foo {
+                a: 5,
+                name: "Hello".into()
+            }
+        );
+        assert_eq!(bar, Bar(3, 6));
+
+        // Clone
+        assert_eq!(foo, foo.clone());
+        assert_eq!(bar, bar.clone());
+    }
+}
